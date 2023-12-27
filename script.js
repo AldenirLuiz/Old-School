bd4c040af12db523451d384d6d6ee850abc57e2d
Aldenir Luiz <aldenir.sky@gmail.com>
2023-12-26 23:08:11 -0300

    sintaxe aded

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const mainsection = document.getElementById('main-header')
+const mainsection = document.getElementById('container-iframe')
 const maingalery = document.getElementById('galery')
 const maintitle = document.getElementById('block-descript')
+var mainframe = document.getElementById('myObject')
+console.log(maintitle)
 
-function expandlink(){
-    maingalery.style.display = 'none'
-    maintitle.style.display = 'none'
+function expandlink(source){
+    maingalery.style.display = 'none';
+    maintitle.style.display = 'none';
+    mainsection.style.display = 'flex';
+    mainframe.style.display = 'flex'
 
-    mainsection.add
+    mainframe.src = source
+    mainframe.width="100%" 
+    mainframe.height="2700" 
+    mainframe.scrolling="no"
+}
 
-    mainsection.innerHTML = '<iframe src="sub/history.html" width="100%" height="2600" scrolling="no"></iframe>';
+function remObject(){
+    mainframe.src = ''
+    mainframe.style.display = "none"
+    maingalery.style.display = 'flex';
+    maintitle.style.display = 'flex';
+    mainsection.style.display = 'none';
 }
