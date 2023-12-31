403a2977c47106f6b5f11d2dca78683a8341b294
Aldenir Luiz <aldenir.sky@gmail.com>
2023-12-30 22:41:52 -0300

    data estructure final

--- teste.py
+++ teste.py
@@ -1,10 +1,3 @@
-# assim podemos declara uma variável do tipo Dict:
-my_dict = dict()
+pi = 3.14159
 
-# ou diretamente usando chaves {}
-my_dict :dict = dict() # o typeHint :dict e opcional
-
-# também podemos declarar diretamente seu conteúdo:
-my_dict = {"nome": "Aldenir", "sobrenome": "Luiz", "idade": 29}
-
-print(my_dict) # resultado no console: {'nome': 'Aldenir', 'sobrenome': 'Luiz', 'idade': 29}
+print(f'O valor de pi é {pi: >10.3}')
